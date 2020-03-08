e127037fe4ac1f18dd0697cea8fe50f34cbadf08
Akhil <akhil.c@healthsignz.com>
2020-03-08 22:57:14 +0530

    small changes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 5555
+const port = 3000
 app.use('/static', express.static('public'))
 // app.use();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.redirect('/static/');
+})
 app.post('/api/calculateEmi', (req, res) => {
     console.log("Request Body --->", req.body);
     /*
@@ -16,31 +18,31 @@ app.post('/api/calculateEmi', (req, res) => {
     timePeriod: timePeriod
     */
     let responseBody = {
-        statuMessage: ''
+        statusMessage: ''
     }
     if (isNaN(req.body.totalPrincipal) || req.body.totalPrincipal <= 0) {
-        responseBody.statuMessage = "Enter totalPrincipal";
+        responseBody.statusMessage = "Enter totalPrincipal";
         console.log(responseBody, "no totalPrincipal")
         res.status(201).send(responseBody)
     }
     else if (isNaN(req.body.rateOfInterest) || req.body.rateOfInterest <= 0) {
-        responseBody.statuMessage = "Enter rateOfInterest";
+        responseBody.statusMessage = "Enter rateOfInterest";
         console.log(responseBody, "no rateOfInterest")
         res.status(201).send(responseBody)
     }
     else if (isNaN(req.body.timePeriod) || req.body.timePeriod <= 0) {
-        responseBody.statuMessage = "Enter timePeriod";
+        responseBody.statusMessage = "Enter timePeriod";
         console.log(responseBody, "no timePeriod")
         res.status(201).send(responseBody)
     }
     else {
-        
+
         // responseBody.emiPerMonth = parseFloat((req.body.totalAmount / req.body.noOfMonths));
         console.log(responseBody, "response body")
-        responseBody.statuMessage = "Please write calculation logic"
-        res.status(200).send(responseBody)
+        responseBody.statusMessage = "Please write calculation logic"
+        res.status(200).send(responseBody);
     }
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`calculateEmi app listening on port ${port}!`))
 
